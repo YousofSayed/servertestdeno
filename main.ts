7e9cda33484c7eba9095bb680bd947af0df9fac6
YousofSayed <88796141+YousofSayed@users.noreply.github.com>
2023-05-10 20:36:26 +0300

    main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ import { App, Router ,staticServe} from "https://deno.land/x/attain@1.1.2/mod.ts
 const app = new App();
 
 app.get("/",(req,res)=>{
-res.send("wlcome in deno deploy 'Yousef'"
+res.send("wlcome in deno deploy 'Yousef'")
 })
 
 app.listen(3000)
