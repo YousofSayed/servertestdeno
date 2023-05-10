1d9e37a0f76d9d0879bf2c0f3482dd953c1ee76f
YousofSayed <88796141+YousofSayed@users.noreply.github.com>
2023-05-10 18:35:53 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,7 @@
 import { App, Router ,staticServe} from "https://deno.land/x/attain@1.1.2/mod.ts";
+
+const app = new App();
+
 app.get("/",(req,res)=>{
 res.send("wlcome in deno deploy 'Yousef'"
 })
