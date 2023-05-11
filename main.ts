eaceb23f1a13987993da2fc2b515642fbf601f5b
YousofSayed <88796141+YousofSayed@users.noreply.github.com>
2023-05-11 05:12:11 +0300

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { App, Router ,staticServe} from "https://deno.land/x/attain@1.1.2/mod.ts";
 
 const app = new App();
-console.log(Deno.readTextFileSync('./index.html')
+console.log(Deno.readTextFileSync('./index.html'))
 app.get("/",(req,res)=>{
 res.sendFile("index.html")
 })
